24fc05a65b90a00c47c4b5456813db18cdd07a01
Arti Dwivedi <artidwivedi0123@gmail.com>
2026-07-06 17:56:21 +0530

    Changes  Regarding Build in Dist Folder

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 import { jsx as n, jsxs as c, Fragment as w } from "react/jsx-runtime";
-const b = "_button_1q19o_20", j = "_primary_1q19o_43", x = "_secondary_1q19o_59", C = "_others_1q19o_70", W = "_danger_1q19o_79", T = "_outline_1q19o_90", B = "_success_1q19o_101", F = "_small_1q19o_111", L = "_medium_1q19o_115", M = "_large_1q19o_119", S = "_full_width_1q19o_129", p = {
+const b = "_button_1q19o_20", x = "_primary_1q19o_43", C = "_secondary_1q19o_59", W = "_others_1q19o_70", T = "_danger_1q19o_79", j = "_outline_1q19o_90", B = "_success_1q19o_101", F = "_small_1q19o_111", L = "_medium_1q19o_115", M = "_large_1q19o_119", S = "_full_width_1q19o_129", p = {
   button: b,
-  primary: j,
-  secondary: x,
-  others: C,
-  danger: W,
-  outline: T,
+  primary: x,
+  secondary: C,
+  others: W,
+  danger: T,
+  outline: j,
   success: B,
   small: F,
   medium: L,
@@ -21,14 +21,14 @@ function $({
   rightIcon: o,
   onClick: i,
   fullWidth: d = !1,
-  buttonType: l,
+  buttonType: _,
   loading: u = !1
 }) {
   const m = `${p.button} ${p[s]} ${p[r]} ${d ? p.fullWidth : ""}`;
   return /* @__PURE__ */ n(
     "button",
     {
-      type: l,
+      type: _,
       className: m,
       disabled: a || u,
       onClick: i,
@@ -40,7 +40,7 @@ function $({
     }
   );
 }
-const z = "_container_cgl1j_20", A = "_label_cgl1j_26", D = "_required_cgl1j_30", E = "_inputWrapper_cgl1j_34", G = "_input_cgl1j_34", H = "_error_cgl1j_56", J = "_helperText_cgl1j_65", _ = {
+const z = "_container_6e5nc_20", A = "_label_6e5nc_26", D = "_required_6e5nc_30", E = "_inputWrapper_6e5nc_34", G = "_input_6e5nc_34", H = "_error_6e5nc_56", J = "_helperText_6e5nc_65", l = {
   container: z,
   label: A,
   required: D,
@@ -58,7 +58,7 @@ function dn({
   helperText: o,
   required: i,
   disabled: d,
-  error: l,
+  error: _,
   leftIcon: u,
   rightIcon: m,
   readOnly: k,
@@ -67,17 +67,17 @@ function dn({
   maxLength: q,
   onChange: v
 }) {
-  return /* @__PURE__ */ c("div", { className: _.container, children: [
-    t && /* @__PURE__ */ c("label", { className: _.label, children: [
+  return /* @__PURE__ */ c("div", { className: l.container, children: [
+    t && /* @__PURE__ */ c("label", { className: l.label, children: [
       t,
-      i && /* @__PURE__ */ n("span", { className: _.required, children: "*" })
+      i && /* @__PURE__ */ n("span", { className: l.required, children: "*" })
     ] }),
-    /* @__PURE__ */ c("div", { className: _.inputWrapper, children: [
+    /* @__PURE__ */ c("div", { className: l.inputWrapper, children: [
       u && /* @__PURE__ */ n("span", { children: u }),
       /* @__PURE__ */ n(
         "input",
         {
-          className: _.input,
+          className: l.input,
           type: e,
           value: s,
           name: a,
@@ -92,8 +92,8 @@ function dn({
       ),
       m && /* @__PURE__ */ n("span", { children: m })
     ] }),
-    o && !l && /* @__PURE__ */ n("small", { className: _.helperText, children: o }),
-    l && /* @__PURE__ */ n("small", { className: _.error, children: l })
+    o && !_ && /* @__PURE__ */ n("small", { className: l.helperText, children: o }),
+    _ && /* @__PURE__ */ n("small", { className: l.error, children: _ })
   ] });
 }
 const K = "_card_luywp_20", O = "_image_luywp_39", P = "_content_luywp_47", Q = "_title_luywp_52", h = {
@@ -131,7 +131,7 @@ function un({
     }
   );
 }
-const R = "_overlay_ous7d_20", U = "_modal_ous7d_30", V = "_footer_ous7d_55", y = {
+const R = "_overlay_ous7d_20", U = "_modal_ous7d_30", V = "_footer_ous7d_55", g = {
   overlay: R,
   modal: U,
   footer: V
@@ -145,19 +145,19 @@ function mn({
   cancelText: o = "Cancel",
   onConfirm: i,
   onClose: d,
-  showFooter: l = !0
+  showFooter: _ = !0
 }) {
-  return e ? /* @__PURE__ */ n("div", { className: y.overlay, children: /* @__PURE__ */ c("div", { className: y.modal, children: [
+  return e ? /* @__PURE__ */ n("div", { className: g.overlay, children: /* @__PURE__ */ c("div", { className: g.modal, children: [
     /* @__PURE__ */ n("h2", { children: s }),
     r && /* @__PURE__ */ n("p", { children: r }),
     a,
-    l && /* @__PURE__ */ c("div", { className: y.footer, children: [
+    _ && /* @__PURE__ */ c("div", { className: g.footer, children: [
       /* @__PURE__ */ n($, { variant: "outline", onClick: d, children: o }),
       /* @__PURE__ */ n($, { variant: "primary", onClick: i, children: t })
     ] })
   ] }) }) : null;
 }
-const X = "_spinner_13tk7_20", Y = "_spin_13tk7_20", Z = "_small_13tk7_32", I = "_medium_13tk7_38", nn = "_large_13tk7_44", en = "_primary_13tk7_51", sn = "_secondary_13tk7_56", rn = "_success_13tk7_61", tn = "_warning_13tk7_66", cn = "_danger_13tk7_71", an = "_dark_13tk7_76", ln = "_light_13tk7_81", _n = "_wrapper_13tk7_87", g = {
+const X = "_spinner_13tk7_20", Y = "_spin_13tk7_20", Z = "_small_13tk7_32", I = "_medium_13tk7_38", nn = "_large_13tk7_44", en = "_primary_13tk7_51", sn = "_secondary_13tk7_56", rn = "_success_13tk7_61", tn = "_warning_13tk7_66", cn = "_danger_13tk7_71", an = "_dark_13tk7_76", _n = "_light_13tk7_81", ln = "_wrapper_13tk7_87", y = {
   spinner: X,
   spin: Y,
   small: Z,
@@ -169,17 +169,17 @@ const X = "_spinner_13tk7_20", Y = "_spin_13tk7_20", Z = "_small_13tk7_32", I = 
   warning: tn,
   danger: cn,
   dark: an,
-  light: ln,
-  wrapper: _n
+  light: _n,
+  wrapper: ln
 };
 function pn({
   size: e = "medium",
   variant: s = "secondary"
 }) {
-  return /* @__PURE__ */ n("div", { className: g.wrapper, children: /* @__PURE__ */ n(
+  return /* @__PURE__ */ n("div", { className: y.wrapper, children: /* @__PURE__ */ n(
     "div",
     {
-      className: `${g.spinner} ${g[e]} ${g[s]}`
+      className: `${y.spinner} ${y[e]} ${y[s]}`
     }
   ) });
 }
